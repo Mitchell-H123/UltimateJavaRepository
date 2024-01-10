a9c07ca843eba52fd8129f9f48c18c36a2950cc1
Mitchell Hoyle <25mitchellhoyle@usd266.com>
2024-01-10 09:35:55 -0600

    finished 1.2

--- Unit01/Topic1_2/Main.java
+++ Unit01/Topic1_2/Main.java
@@ -2,7 +2,13 @@ package Unit01.Topic1_2;
 
 public class Main {
   public static void main(String[] args) {
-     
+     int marbles =3;
+     double testAvg=90;
+     boolean sugar=true;
+     final int tires=4;
+     System.out.println(marbles);
+     System.out.println(tires);
+     System.out.println(sugar);
   }
   
 }
--- Unit01/Topic1_2/README.md
+++ Unit01/Topic1_2/README.md
@@ -4,3 +4,14 @@ boolean
 int
 double
 string
+
+varibles cant start with a digit
+cant use characters such as $ @ &
+
+java varibles declaring are the same as c++
+
+final = something that wont change
+ex. final double PI;
+for final variables use ALL CAPS and underscores to name them
+the same as const in c++
+
