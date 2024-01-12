9296cacc19c309a2a18da4618fca097f3c75a76d
Mitchell Hoyle <25mitchellhoyle@usd266.com>
2024-01-12 10:21:35 -0600

    changed distance

--- Unit01/Xtras/DistanceFormula.java
+++ Unit01/Xtras/DistanceFormula.java
@@ -3,8 +3,8 @@ package Unit01.Xtras;
 public class DistanceFormula {
     public static void main(String[] args) {
         int x1 = Integer.parseInt(args[0]);
-        int x2 = Integer.parseInt(args[1]);
-        int y1 = Integer.parseInt(args[2]);
+        int y1 = Integer.parseInt(args[1]);
+        int x2 = Integer.parseInt(args[2]);
         int y2 = Integer.parseInt(args[3]);
         double d=Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1), 2));
         System.out.println(d);
