d837f62256b9b41d687b0af3cfc4bcc0d5fd7262
Mitchell Hoyle <25mitchellhoyle@usd266.com>
2024-01-31 09:24:15 -0600

    3.5

--- Unit03/Topic3_5/Main.java
+++ Unit03/Topic3_5/Main.java
@@ -2,7 +2,17 @@ package Unit03.Topic3_5;
 
 public class Main {
   public static void main(String[] args) {
-
+    boolean cake=true;
+    boolean icecream=false;
+    if (cake&&icecream){
+      System.out.println("Awesome party");
+    }
+    else if(cake||icecream){
+      System.out.println("Good party");
+    }
+    else{
+      System.out.println("Bad party");
+    }
   }
 
 }
--- Unit03/Topic3_5/README.md
+++ Unit03/Topic3_5/README.md
@@ -1,1 +1,13 @@
 # 3.5 - Compound Boolean Expressions
+
+nested if statements are if statements in if statements (same as c++)
+
+else belongs to the closest if 
+tabs dont matter with ifs and elses 
+must use { } to tell the program what the else goes with
+
+! = not
+&& = and
+|| = or
+(same as c++)
+
