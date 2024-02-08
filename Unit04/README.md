4c27ba2d51ff2a830a5743a9afad5535c9b14658
Mitchell Hoyle <25mitchellhoyle@usd266.com>
2024-02-08 09:42:04 -0600

    4.2

--- Unit04/README.md
+++ Unit04/README.md
@@ -11,6 +11,20 @@ prevent infinit loop
 
 ## 4.2 - `for` Loops
 
+for(initialization; boolean expression; update){
+
+}
+-same as c++
+
+for loop is better for a defined num of times
+while is better for repeating based on an unknown amount of times
+while loop is better for a more complex increment or decrement
+
+common error for for loops= off by one error (OBOE)
+make sure it is doing what it is ment to do
+
+
+
 ## 4.3 - Developing Algorithms Using Strings
 
 ## 4.4 - Nested Iteration
--- Unit04/Topic4_2ForLoops.java
+++ Unit04/Topic4_2ForLoops.java
@@ -2,7 +2,19 @@ package Unit04;
 
 public class Topic4_2ForLoops {
     public static void main(String[] args) {
-
+        for(int i=1;i<=5;i++){
+            System.out.println(i);
+        }
+        for(int num=0;num<=10;num++){
+            System.out.print(num*3+" ");
+        }
+        System.out.println("\n");
+        int years=50;
+        double total=200;
+        for(int year=1;year<=years;year++){
+            total=total+(total*.1);
+            System.out.println("$"+total);
+        }
     }
 }
 
