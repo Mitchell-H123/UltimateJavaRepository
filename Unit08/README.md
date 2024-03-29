92c19c067a516c454c5d25b81ab7885453982396
Mitchell Hoyle <25mitchellhoyle@usd266.com>
2024-03-29 10:35:16 -0500

    8.1

--- Unit08/README.md
+++ Unit08/README.md
@@ -2,4 +2,20 @@
 
 ## 8.1 - 2D Array
 
+its an array of arrays
+
+dataType [][] nameOf2DArr
+-use [][] with 2d arrays like c++ but different
+
+new dataType [rows][columns];
+-row then columns
+
+use length to get rows
+-EX rows=grades.length
+
+col length:
+-EX col=grades[0].length
+
+dataType [][] name= new dataType[r][c]
+
 ## 8.2 - Transversing 2D Arrays
--- Unit08/Topic8_1_2DArrays.java
+++ Unit08/Topic8_1_2DArrays.java
@@ -3,6 +3,19 @@ package Unit08;
 public class Topic8_1_2DArrays {
 
     public static void main(String[] args) {
+        int [] student0={100,65,96,54};
+        int [] student1={97,63,96,88};
+        int [] student2={100,100,96,97};
+        int [] student3={100,99,96,99};
 
+        int [][] grades={student0,student1,student2,student3};
+        grades[2][0]=89;
+        grades[0][0]=84;
+        for(int i=0;i<grades.length;i++){
+            for(int j=0;j<grades[0].length;j++){
+                System.out.print(grades[i][j]+", ");
+            }
+            System.out.println("");
+        }
     }
 }
