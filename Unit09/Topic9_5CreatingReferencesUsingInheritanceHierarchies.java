b8f57df0eac51453eb16c922d9025b1a770b462a
Mitchell Hoyle <25mitchellhoyle@usd266.com>
2024-04-17 09:44:23 -0500

    added code to 9.5

--- Unit09/Topic9_5CreatingReferencesUsingInheritanceHierarchies.java
+++ Unit09/Topic9_5CreatingReferencesUsingInheritanceHierarchies.java
@@ -7,5 +7,10 @@ public class Topic9_5CreatingReferencesUsingInheritanceHierarchies {
         list[1]=new performer9_3("jeff", 0);
         System.out.println(list[0].getName()+" is "+list[0].perform());
         System.out.println(list[1].getName()+" is "+list[1].perform());
+
+
+        performer9_3 afafd=new comedian9_3("aahfdagfagfa", 0);
+        list[2]=afafd;
+        System.out.println(list[2].getName()+" is "+list[2].perform());
     }
 }
