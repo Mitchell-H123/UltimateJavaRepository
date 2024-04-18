e3349e39279f9ad4d617414bd4b0bfdc71074636
Mitchell Hoyle <25mitchellhoyle@usd266.com>
2024-04-18 09:41:23 -0500

    10.1

--- Unit10/README.md
+++ Unit10/README.md
@@ -2,4 +2,10 @@
 
 ## 10.1 - Recursion
 
+recrusive method is a method that calls itself
+
+(wont have to write a recursive code on ap exam but might have to complete one)
+
+after the method is done it returns to the previous method calls
+
 ## 10.2 - Recursive Searching and Sorting
--- Unit10/Topic10_1Recursion.java
+++ Unit10/Topic10_1Recursion.java
@@ -1,7 +1,36 @@
 package Unit10;
 
 public class Topic10_1Recursion {
-    public static void main(String[] args) {
 
+    public static void simpleRecur(int n){
+        System.out.println(n);
+        if(n>2){
+            simpleRecur(n-1);
+        }
+        System.out.println(n);
+    }
+
+    public static void simpleRecur2(int n){
+        System.out.println(n);
+        if(n>2){
+            simpleRecur(n+1);
+        }
+        System.out.println(n);
+    }
+
+    public static int simpleRecur3(int n){
+        System.out.println(n);
+        if(n==0){
+            return 0;
+        }
+        return n+ simpleRecur3(n/2);
+    }
+
+    public static void main(String[] args) {
+        simpleRecur(4);
+        System.out.println("");
+        simpleRecur2(4);
+        System.out.println("");
+        System.out.println(simpleRecur3(8));
     }
 }
