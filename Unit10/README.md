583b5478ef5c4d0f8fd1156984987892f922a3be
Mitchell Hoyle <25mitchellhoyle@usd266.com>
2024-04-19 10:25:04 -0500

    10.2

--- Unit10/README.md
+++ Unit10/README.md
@@ -9,3 +9,15 @@ recrusive method is a method that calls itself
 after the method is done it returns to the previous method calls
 
 ## 10.2 - Recursive Searching and Sorting
+
+dont use loops when using recursive calls
+
+binary search: starts at middle
+linear search: starts at beginning 
+-these can be done with loops also and dont have to be done recursivley 
+-used to sort elements in array lists
+
+merge sort: left side of arraylist, then right, then merge them together
+-sorts elements
+-divides and conquers
+
--- Unit10/Topic10_2RecursiveSearchingAndSorting.java
+++ Unit10/Topic10_2RecursiveSearchingAndSorting.java
@@ -1,7 +1,23 @@
 package Unit10;
-
+import java.util.ArrayList;
 public class Topic10_2RecursiveSearchingAndSorting {
-    public static void main(String[] args) {
 
+    public void mergeSort(ArrayList arr,int low,int high){
+        if(low<high){
+            int middle=(low+high)/2;
+            mergeSort(arr, low, middle);
+            mergeSort(arr, middle+1, high);
+            mergeSort(arr, low, high);
+        }
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> a=new ArrayList<Integer>();
+        a.add(3);
+        a.add(-5);
+        a.add(1);
+        a.add(9);
+        a.add(5);
+        a.add(3);
+        //mergeSort(a,0,10);
     }
 }
